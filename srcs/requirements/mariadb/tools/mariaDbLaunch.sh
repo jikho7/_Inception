9f268b5a23e9070378df0d2e6f984993dc9f42b7
jikho7 <defayes.julie@gmail.com>
2024-10-22 18:11:03 +0200

    kjshfishf

--- srcs/requirements/mariadb/tools/mariaDbLaunch.sh
+++ srcs/requirements/mariadb/tools/mariaDbLaunch.sh
@@ -23,17 +23,22 @@ GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 
+sleep 1
+
 # mysqld = necessaire pour lancer le service mariadb = cuisto. & permet de lancer en arriere plan, a mettre a la fin de la commande
 mysqld --user=mysql &
 
+sleep 1
 # execute ce qui est dans le script db_file, setting up de mariadb, creation des users, password etc.
 mysql < /db_file
 
+sleep 1
 #supprimer mysqld sinon deux process en cours et ne fonctionne pas, killall + nom_du_process
 killall mysqld
 
+sleep 1
 # lance le service en foreground, il est bloquant 
-½exec /usr/bin/mysqld --user=mysql --console 
+exec /usr/bin/mysqld --user=mysql --console 
 
 # NOTE mysql serveur et mysqld, deamon, pour le service
 # mysql = intermediaire entre client et la base de donnee
